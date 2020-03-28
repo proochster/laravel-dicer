7ceddbe021bc928ef81597332f96269b186b501d
proochster <proochster@gmail.com>
2020-03-28 15:12:55 +0000

    ^ Simplified User Migration and Faker

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Room::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'hash' => substr(sha1(time()), 0, 6) // Get 6 first characters from the hashed time stamp
+        'hash' => Str::random(6)
     ];
 });

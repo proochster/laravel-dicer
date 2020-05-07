a5863673f4f74dc0c0a52547a470f778dd10b760
proochster <proochster@gmail.com>
2020-05-07 15:37:38 +0100

    + Room model relationships

--- app/Room.php
+++ app/Room.php
@@ -10,4 +10,26 @@ class Room extends Model
     protected $fillable = [
         'name', 'hash'
     ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class, 'id', 'toRoom');
+    }
+
+    public function links()
+    {
+        return $this->hasMany(Link::class, 'id', 'toRoom');        
+    }
+
+    // this is a recommended way to declare event handlers
+    public static function boot() {
+        parent::boot();
+
+        static::deleting(function($room) {
+
+            $room->messages()->delete();
+            $room->links()->delete();
+
+        });
+    }
 }

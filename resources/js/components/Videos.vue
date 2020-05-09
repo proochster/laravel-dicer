5e6dbadc3d2061290cb0be427d1d806979c6f464
proochster <proochster@gmail.com>
2020-05-09 12:39:27 +0100

    ^ Videos UI

--- resources/js/components/Videos.vue
+++ resources/js/components/Videos.vue
@@ -4,11 +4,27 @@
         <div :class="{ 'd-none': hideVideo}" >
             <div id="player" class="w-100"></div>
         </div>
+        
+        <div v-if="hideVideo" class="card video-guide small mb-4 text-white-50">
+            <div class="card-header">
+                <h5 class="mb-0">Videos</h5>
+            </div>
+            <div class="card-footer">
+                Use only the ID from the video URL. <br>
+                It's the last part of the address: <br>
+                <span class="text-monospace font-italic">https://www.youtube.com/watch?v=<span class="font-weight-bold text-light">abc123ABC456</span></span>
+            </div>
+        </div>
 
         <ul class="list-inline">
             <li v-for="video in videos" :key="video.id" class="d-flex align-items-center mb-1">
-                <a class="btn flex-fill text-capitalize text-info text-left" :class="{ 'btn-outline-info shadow': video.url == selected }" @click="sendPlayVideo(video.url)" title="Play this video in the room">{{video.title}}</a>
-                <a class="btn btn-sm" @click="sendPauseVideo(video.url)" title="Pause this video">Pause</a>
+                <a v-if="video.url != selected" class="btn flex-fill text-capitalize text-info text-left" @click="sendPlayVideo(video.url)" title="Play this video in the room">
+                     <img src="/images/icon-play.svg" class="pr-1" alt="Play" height="12">
+                    {{video.title}}
+                </a>
+                <a  v-if="video.url == selected" class="btn flex-fill text-capitalize text-info text-left" :class="{ 'btn-outline-info shadow': video.url == selected }" @click="sendPauseVideo(video.url)" title="Pause this video">
+                    <img src="/images/icon-pause.svg" class="pr-1" alt="Pause" height="10">
+                    {{video.title}}</a>
                 <a class="btn btn-sm text-danger" @click="removeVideo(video.id)" title="Remove this video">x</a>
             </li>            
             <li v-if="!videos.length">Add first video.</li>
@@ -16,9 +32,7 @@
             
         <hr>
 
-        <div class="video-guide small mb-4 text-white-50">
-            Use only the ID from the video URL. It's the last part of the address: <span class="text-monospace font-italic">https://www.youtube.com/watch?v=<span class="font-weight-bold text-light">abc123ABC456</span></span>
-        </div>
+
 
         <div class="link-form row">
             <div class="form-group col-6">
@@ -72,6 +86,7 @@ export default {
             })
             .listen('PauseVideo', e => {
                 this.pauseVideo(e.videoUrl);
+                this.selected = undefined;
             });
 
         /**

b957d6d4c49fbd91e705a262961244b3e193b589
proochster <proochster@gmail.com>
2020-10-18 22:35:30 +0100

    + Dice menu styles

--- resources/js/components/Dice.vue
+++ resources/js/components/Dice.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="dice dice-button" :data-d="d">
-        <img :src="imageLink" :alt="d" height="50">
-        <span @click="rollDice(1)">{{d}}</span>
-
-        <ul class="dice-menu">
-            <li @click="rollDice(2)">x 2</li>
-            <li @click="rollDice(3)">x 3</li>
-            <li @click="rollDice(4)">x 4</li>
-            <li @click="rollDice(5)">x 5</li>
-            <li @click="rollDice(6)">x 6</li>
-            <li @click="rollDice(7)">x 7</li>
-            <li @click="rollDice(8)">x 8</li>
-            <li @click="rollDice(9)">x 9</li>
+    <div class="dice-menu" :data-d="d">
+        <picture class="dice-button" @click="rollDice(1)">
+            <img :src="imageLink" :alt="d" height="50">
+            <span>{{d}}</span>
+        </picture>
+        <ul class="dice-rolls">
             <li @click="rollDice(10)">x 10</li>
+            <li @click="rollDice(9)">x 9</li>
+            <li @click="rollDice(8)">x 8</li>
+            <li @click="rollDice(7)">x 7</li>
+            <li @click="rollDice(6)">x 6</li>
+            <li @click="rollDice(5)">x 5</li>
+            <li @click="rollDice(4)">x 4</li>
+            <li @click="rollDice(3)">x 3</li>
+            <li @click="rollDice(2)">x 2</li>
         </ul>
     </div>
 </template>

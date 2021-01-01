57d2130879db05a6f7abd877404b7292b9a4c752
proochster <proochster@gmail.com>
2021-01-01 14:00:49 +0000

    ^ Cron test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('clean:db')->everyMinute();
+        // $schedule->command('messages:clear-old')->dailyAt('23:55');
     }
 
     /**

d3b27d0d8bac1f220777349437da3ae7beb753ba
kubap <proochster@gmail.com>
2026-01-01 14:42:51 +0000

    refactor: clean up AddName component structure and formatting

--- resources/js/components/AddName.vue
+++ resources/js/components/AddName.vue
@@ -3,14 +3,29 @@
         <div class="modal fade show d-block" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Add name</h5>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <input type="text" class="form-control" v-model="name" @keydown.enter="sendName" placeholder="Your name">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Add name</h5>
                     </div>
-                </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input
+                                type="text"
+                                class="form-control"
+                                v-model="name"
+                                @keydown.enter="sendName"
+                                placeholder="Your name"
+                            />
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-primary"
+                            @click="sendName"
+                        >
+                            Ok
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -22,18 +37,18 @@
 export default {
     data() {
         return {
-            name: ''
-        }
+            name: ""
+        };
     },
     methods: {
-       sendName(){
-           if( this.name == '' ){
-               return;
-           }
+        sendName() {
+            if (this.name == "") {
+                return;
+            }
 
-           this.$emit('send', this.name);
-           this.name = '';
-       } 
+            this.$emit("send", this.name);
+            this.name = "";
+        }
     }
-}
+};
 </script>

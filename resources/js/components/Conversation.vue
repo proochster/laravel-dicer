0e4f476f43de86875e368556e79ec8cf41d0bfed
kubap <proochster@gmail.com>
2026-01-01 14:34:11 +0000

    feat: update SVG graphics and enhance conversation component
    - Modified d12.svg to update the design and colors of the SVG paths.
    - Added new SVG files: d2.svg and d20.svg for additional graphical elements.
    - Refactored Conversation.vue to improve code readability and maintainability, including formatting changes and cleanup of commented-out code.
    - Ensured proper handling of dice rolls in the sendMessage method.

--- resources/js/components/Conversation.vue
+++ resources/js/components/Conversation.vue
@@ -3,20 +3,24 @@
         <div class="scroll" ref="scroll">
             <div class="scroll-content">
                 <ul class="mr-3">
-                    <li v-for="message in messages" :key="message.id" :class="[message.id, {'self': userID == message.from}]" >
+                    <li
+                        v-for="message in messages"
+                        :key="message.id"
+                        :class="[message.id, { self: userID == message.from }]"
+                    >
                         <Message :m="message" :key="message.id"></Message>
                     </li>
                 </ul>
             </div>
         </div>
         <div class="diceset">
-            <!-- <Dice :d="2" @e-messagecomposed="sendMessage"></Dice> -->
+            <Dice :d="2" @e-messagecomposed="sendMessage"></Dice>
             <Dice :d="4" @e-messagecomposed="sendMessage"></Dice>
             <Dice :d="6" @e-messagecomposed="sendMessage"></Dice>
             <Dice :d="8" @e-messagecomposed="sendMessage"></Dice>
             <Dice :d="10" @e-messagecomposed="sendMessage"></Dice>
             <Dice :d="12" @e-messagecomposed="sendMessage"></Dice>
-            <!-- <Dice :d="20" @e-messagecomposed="sendMessage"></Dice> -->
+            <Dice :d="20" @e-messagecomposed="sendMessage"></Dice>
             <Dice :d="100" @e-messagecomposed="sendMessage"></Dice>
         </div>
         <Composer @e-messagecomposed="sendMessage"></Composer>
@@ -30,7 +34,7 @@ import Message from "./Message";
 
 export default {
     props: {
-        room:{
+        room: {
             type: String,
             required: true
         },
@@ -51,24 +55,14 @@ export default {
             required: true
         }
     },
-    
+
     methods: {
-        
         sendMessage(text, diceType, diceArray) {
-
-            // console.log(text, diceType, diceArray);
-
-            // console.log(diceArray);
-            // let rolls = [];
             let rolls_roll = [];
 
-            if (diceArray != undefined){
-                // for (let i = 0; i < diceArray.length; i++) {
-                //     rolls[i] = diceArray[i];
-                // }
-                
+            if (diceArray != undefined) {
                 for (let i = 0; i < diceArray.length; i++) {
-                    rolls_roll[i] = {'dice_roll':diceArray[i]};
+                    rolls_roll[i] = { dice_roll: diceArray[i] };
                 }
             }
 
@@ -76,48 +70,47 @@ export default {
                 userHash: this.userToken,
                 text: text,
                 diceType: diceType,
-                // diceRoll: diceRoll,
                 dice_rolls: rolls_roll,
-                user:{
-                    name: this.userName,
+                user: {
+                    name: this.userName
                 },
                 from: this.userID
             });
-            
-            // console.log('Pushed message locally');
 
-            // console.log('Sent message');
-            axios.post('/api/messages', {
-                userHash: this.userToken,
-                text: text,
-                diceType: diceType,
-                // diceRoll: diceRoll,
-                dice_rolls: rolls_roll,
-                roomHash: this.room
-            })
-            .then(response => {
-                // this.$emit('e-new', response.data);                
-                // console.log('Message saved. Server responded' + response);
-            })
-            .catch(function (error) {         
-                console.log(error);            
-            });
+            axios
+                .post("/api/messages", {
+                    userHash: this.userToken,
+                    text: text,
+                    diceType: diceType,
+                    dice_rolls: rolls_roll,
+                    roomHash: this.room
+                })
+                .then(response => {
+                    // Message sent
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
         },
-        scrollToBottom(){
-            setTimeout(()=>{
-                this.$refs.scroll.scrollTop = this.$refs.scroll.scrollHeight - this.$refs.scroll.clientHeight;
+        scrollToBottom() {
+            setTimeout(() => {
+                this.$refs.scroll.scrollTop =
+                    this.$refs.scroll.scrollHeight -
+                    this.$refs.scroll.clientHeight;
             }, 50);
-        },
+        }
     },
 
     watch: {
-        messages(messages){
+        messages(messages) {
             this.scrollToBottom();
         }
     },
 
     components: {
-        Composer, Dice, Message
-    },
-}
+        Composer,
+        Dice,
+        Message
+    }
+};
 </script>
